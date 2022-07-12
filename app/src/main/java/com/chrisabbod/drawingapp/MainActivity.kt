d062bd23bc3d922ce583fabba2e00851fe5f67a5
chris <chrisabbod@gmail.com>
2022-07-12 15:15:01 -0400

    Add undo button and functionality

--- app/src/main/java/com/chrisabbod/drawingapp/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/drawingapp/MainActivity.kt
@@ -82,6 +82,11 @@ class MainActivity : AppCompatActivity() {
             showBrushSizeSelectionDialog()
         }
 
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
+        ibUndo.setOnClickListener {
+            drawingView?.onClickUndo()
+        }
+
         val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
         ibGallery.setOnClickListener {
             requestStoragePermission()
